a2a01d7f87fe2dd60eec0487ba62d4bc3c0b1b0b
AndrzejKrzywda00 <a.krzywda00@gmail.com>
2022-11-10 14:46:49 +0100

    scenario 1 done

--- ns-3.33/scratch/analysis_project/scenarios/delivery_time_distribution.py
+++ ns-3.33/scratch/analysis_project/scenarios/delivery_time_distribution.py
@@ -1,59 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from model.DataPacket import DataPacket
 from data_access.DataProvider import DataProvider
-from model.Status import Status
+from model.DataPacket import DataPacket
 from transmission.TransmissionSimulator import TransmissionSimulator
 
-
+# Scenario 1
 if __name__ == '__main__':
 
     test_size = 1000
     simulation_time_in_hours = 8
-    nanobot_number = 800
+    nanobot_number = 1000
 
     provider = DataProvider('data/number_of_nanobots/results-{}.csv'.format(nanobot_number))
     nanobot_map = provider.get_nanobots_map()
     blood_vessels_map = provider.get_blood_vessels_map()
 
     data_sent = []
-    data_sent_for_test = dict()
+    flow_map = {}
+    records = []
 
     for i in range(test_size):
-        data_sent_for_test[i] = []
-        for nanobot_id in nanobot_map:
-            records = nanobot_map[nanobot_id]
-            last_packet = DataPacket()
-            for record in records:
-                simulator = TransmissionSimulator(record, blood_vessels_map[record.blood_vessel_id])
-                if record.is_from_datasource_to_nanobot():
-                    if simulator.will_transmit_from_data_source_to_nanobot():
-                        last_packet = DataPacket()
-                        last_packet.set(record)
-                if record.is_from_nanobot_to_access_point():
-                    if simulator.will_transmit_from_nanobot_to_access_point():
-                        if last_packet.status == Status.SENT:
-                            last_packet.complete(record)
-                            data_sent_for_test[i].append(last_packet)
-
-    # finding first packet delivered for each nanobot
-    for key in data_sent_for_test.keys():
-        packets = data_sent_for_test[key]
-        min_time = simulation_time_in_hours * 60 * 60
-        min_packet = None
-        for packet in packets:
-            if packet.delivery_time(wait_time) <= min_time:
-                min_time = packet.delivery_time(wait_time)
-                min_packet = packet
-        if min_packet is not None:
-            data_sent.append(min_packet)
+        flow_map.clear()
+        for record in records:
+            simulator = TransmissionSimulator(record, blood_vessels_map[record.blood_vessel_id])
+            if record.is_from_datasource_to_nanobot():
+                if simulator.will_transmit_from_data_source_to_nanobot():
+                    packet = DataPacket()
+                    packet.set(record)
+                    flow_map[packet.nanobot_id] = packet
+            if record.is_from_nanobot_to_access_point():
+                if simulator.will_transmit_from_nanobot_to_access_point():
+                    if record.nanobot_id in flow_map.keys():
+                        packet = flow_map[record.nanobot_id]
+                        packet.complete(record)
+                        data_sent.append(packet)
+                        break
 
     # preparing and showing results
     sampling_depth = 2
     histogram_width = 60 * 60 / sampling_depth
     widths = [[i * histogram_width, (i+1) * histogram_width] for i in range(simulation_time_in_hours * sampling_depth)]
-    delivery_times = [data.delivery_time(wait_time) for data in data_sent]
+    delivery_times = [data.delivery_time() for data in data_sent]
     histogram = dict()
 
     for time in delivery_times:
@@ -67,7 +55,7 @@ if __name__ == '__main__':
 
     # Probability density
     figure, axis = plt.subplots()
-    values = [int(histogram[key]) for key in histogram.keys()]
+    values = [histogram[key] / test_size for key in histogram.keys()]
     x = [key / 3600 for key in list(histogram.keys())]
     axis.bar(x, values, linewidth=1.0, edgecolor='black', width=0.5)
     axis.set_ylabel("Number of packets delivered")
